292c8fe693df1ff731507637ef14587651a51e18
Marcos Santos <marcossamuel1710@outlook.com>
2019-07-12 14:27:30 -0300

    projeto criado

---
+++ src/Logica.js
@@ -0,0 +1,116 @@
+// criar o campo 
+const createBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
+            return {
+                row,
+                column,
+                opened: false,
+                flagged: false,
+                mined: false,
+                exploded: false,
+                nearMines: 0
+            }
+        })
+    })
+}
+
+// espalha as minhas 
+const spreadMines = (board, minesAmount) => {
+    const rows = board.length
+    const columns = board[0].length
+    let minesPlanted = 0
+    
+    while (minesPlanted < minesAmount) {
+        const rowSel = parseInt(Math.random() * rows, 10)
+        const columnSel = parseInt(Math.random() * columns, 10)
+
+        if (!board[rowSel][columnSel].mined) {
+            board[rowSel][columnSel].mined = true
+            minesPlanted++
+        }
+    }
+}
+
+// cria as minas no campo 
+const createMinedBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, columns)
+    spreadMines(board, minesAmount)
+    return board
+}
+
+// clona o campo
+const cloneBoard = board => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return { ...field }
+        })
+    })
+}
+
+// busca a vizinhança 
+const getNeighbors = (board, row, column) => {
+    const neighbors = []
+    const rows = [row - 1, row, row + 1]
+    const columns = [column - 1, column, column + 1]
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const different = r !== row || c !== column
+            const validRow = r >= 0 && r < board.length
+            const validColumn = c >= 0 && c < board[0].length
+            if (different && validRow && validColumn) {
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+    return neighbors
+}
+
+// verifica se a vizinhança é segura 
+const safeNeighborhood = (board, row, column) => {
+    const safes = (result, neighbor) => result && !neighbor.mined
+    return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+// abrir o campo no momento do click e recursivamente caso a vizinhança se ja segura 
+const openField = (board, row, column) => {
+    const field = board[row][column]
+    if (!field.opened) {
+        field.opened = true
+        if (field.mined) {
+            field.exploded = true
+        } else if (safeNeighborhood(board, row, column)) {
+            getNeighbors(board, row, column)
+                .forEach(n => openField(board, n.row, n.column))
+        } else {
+            const neighbors = getNeighbors(board, row, column)
+            field.nearMines = neighbors.filter(n => n.mined).length
+        }
+    }
+}
+
+const fields = board => [].concat(...board)
+
+const hadExplosion = board => fields(board).filter(field => field.exploded).length > 0
+
+const pendding = field => (field.mined && !field.flagged) || (!field.mined && !field.opened)
+
+const wonGame = board => fields(board).filter(pendding).length === 0
+
+const showMines = board => fields(board).filter(field => field.mined).forEach(
+    field => field.opened = true
+)
+
+const invertFlag = (board, row, column) => {
+    const field = board[row[column]]
+    field.flagged = !field.flagged
+}
+
+export {  createMinedBoard,
+          cloneBoard,
+          openField,
+          hadExplosion,
+          wonGame,
+          showMines,
+          invertFlag
+       }
